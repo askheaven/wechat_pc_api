adebbfb8e1fb511b121e32adc1cb337e53c2da56
askheaven <278842015@qq.com>
2022-01-04 21:53:31 +0800

    微信

--- samples/python/demo.py
+++ samples/python/demo.py
@@ -4,53 +4,57 @@ from __future__ import unicode_literals
 import wechat
 import json
 import time
-from wechat import WeChatManager, MessageType
+from wechat import WeChatManager, MessageType, WxidConst
+import traceback
+import os
+from playsound import playsound
 
-wechat_manager = WeChatManager(libs_path='../../libs')
-
-
-# 这里测试函数回调
-@wechat.CONNECT_CALLBACK(in_class=False)
-def on_connect(client_id):
-    print('[on_connect] client_id: {0}'.format(client_id))
-
-
-@wechat.RECV_CALLBACK(in_class=False)
-def on_recv(client_id, message_type, message_data):
-    print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
-                                                                            message_type, json.dumps(message_data)))
-
-
-@wechat.CLOSE_CALLBACK(in_class=False)
-def on_close(client_id):
-    print('[on_close] client_id: {0}'.format(client_id))
+current_path = os.path.abspath(__file__)
+father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
+os.chdir(father_path)
 
 
 # 这里测试类回调， 函数回调与类回调可以混合使用
 class LoginTipBot(wechat.CallbackHandler):
 
-    @wechat.RECV_CALLBACK(in_class=True)
-    def on_message(self, client_id, message_type, message_data):
-        # 判断登录成功后，就向文件助手发条消息
-        if message_type == MessageType.MT_USER_LOGIN:
-            time.sleep(2)
-            wechat_manager.send_text(client_id, 'filehelper', '😂😂😂\uE052该消息通过wechat_pc_api项目接口发送')
-            
-            wechat_manager.send_link(client_id, 
-            'filehelper', 
-            'wechat_pc_api项目', 
-            'WeChatPc机器人项目', 
-            'https://github.com/smallevilbeast/wechat_pc_api', 
-            'https://www.showdoc.com.cn/server/api/attachment/visitfile/sign/0203e82433363e5ff9c6aa88aa9f1bbe?showdoc=.jpg)')
+	my_wxid = {}
+
+	@wechat.RECV_CALLBACK(in_class=True)
+	def on_message(self, client_id, message_type, message_data):
+		# 判断登录成功后，就向文件助手发条消息
+		if message_type == MessageType.MT_USER_LOGIN:
+			self.my_wxid[client_id] = message_data.get("wxid")
+
+		# 过滤非消息
+		if not isinstance(message_data, dict):
+			print(message_type)
+			return
+		# 获取群聊关注信息
+		if self.is_room_msg(message_data) and message_data.get("room_wxid") in WxidConst.FORCE_ROOM:
+			if message_type == MessageType.MT_RECV_LINK_MSG and "餐品赠送" in message_data.get("raw_msg", ""):
+				playsound("C:\Windows\Media\Ring03.wav")
+		# 发送消息
+		if message_data.get("from_wxid") == self.my_wxid[client_id]:
+			print(message_type, message_data)
+		if message_data.get("from_wxid") in WxidConst.FORCE_WX:
+			print(message_type, message_data)
+
+
+	def is_room_msg(self, message_data):
+		if message_data.get("msg", "").endswith("@chatroom"):
+			return True
+		return False
 
 
 if __name__ == "__main__":
-    bot = LoginTipBot()
+	bot = LoginTipBot()
 
-    # 添加回调实例对象
-    wechat_manager.add_callback_handler(bot)
-    wechat_manager.manager_wechat(smart=True)
+	wechat_manager = WeChatManager(libs_path='../../libs')
 
-    # 阻塞主线程
-    while True:
-        time.sleep(0.5)
+	# 添加回调实例对象
+	wechat_manager.add_callback_handler(bot)
+	wechat_manager.manager_wechat(smart=True)
+
+	# 阻塞主线程
+	while True:
+		time.sleep(0.5)
